d2d3cbb4c42c01c18a607e14786ce493c5d11096
hackster254 <43937812+hackster254@users.noreply.github.com>
2020-03-07 03:08:57 +0300

    Update gitConfig.php

--- gitConfig.php
+++ gitConfig.php
@@ -13,7 +13,7 @@ require_once 'src/Github_OAuth_Client.php';
  */
 $clientID         = '8b8ea3c72941b0cf8201';//'Your_App_Client_ID';
 $clientSecret     = 'a6605d8e3b1c12f1aec6924dca9c3cc134870e19';//'Your_App_Client_Secret';
-$redirectURL     = 'https://hackster254.github.io';//https://www.codexworld.com/github-login/';
+$redirectURL     = 'https://hackster254.com';//https://www.codexworld.com/github-login/';
 
 $gitClient = new Github_OAuth_Client(array(
     'client_id' => $clientID,
